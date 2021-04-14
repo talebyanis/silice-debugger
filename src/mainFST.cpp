7116a1483d9265a7462ab3670452a743dd8e89f3
antoine <antoine.raoult3@etu.univ-lorraine.fr>
2021-04-14 14:15:06 +0200

    fst main test

---
+++ src/mainFST.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <LibSL.h>
+#include "../libs/fstapi/fstapi.h"
+
+int mainFST() {
+    void *g_Wave = fstReaderOpen("/home/antoine/IdeaProjects/FSTApiTest/icarus.fst");
+    struct fstHier *hier = fstReaderIterateHier(g_Wave);
+    do {
+        switch (hier->htyp) {
+            case FST_HT_SCOPE:
+                std::cerr << "\nscope : " << hier->u.scope.name << std::endl;
+                break;
+            case FST_HT_ATTRBEGIN:
+                //std::cerr << "attr  : " << hier->u.attr.name << std::endl;
+                break;
+            case FST_HT_VAR:
+                std::cerr << "signal: " << hier->u.var.name << std::endl;
+                std::cerr << fstReaderGetStartTime(hier) << std::endl;
+                break;
+            default:
+                //std::cerr << "unknown" << std::endl;
+                break;
+        }
+        hier = fstReaderIterateHier(g_Wave);
+    } while (hier != NULL);
+
+    fstReaderSetFacProcessMaskAll(g_Wave);
+
+
+    return 0;
+}
