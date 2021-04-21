a5c3145bf13172bad25595aafe7f23ee31acb4af
antoine <antoine.raoult3@etu.univ-lorraine.fr>
2021-04-21 11:56:38 +0200

    drag & drop plots

--- src/FSTWindow.cpp
+++ src/FSTWindow.cpp
@@ -86,10 +86,8 @@ void FSTWindow::showPlots() {
     ImGui::BeginGroup();
     //reset hover to not change the color of a name if no plot is hovered
     hover = 0;
-    for (int i=0; i<g_Plots.size(); i++) {
+    for (int i = 0; i < g_Plots.size(); i++) {
         auto item = g_Plots[i];
-        ImGui::PushID(i);
-        ImGui::BeginChild(item.name.c_str(), ImVec2(wSize.x - 20, 100));
         //set the plots Y limits to just below the lowest value to just upper the highest
         double max = *std::max_element(item.y_data.begin(), item.y_data.end());
         ImPlot::SetNextPlotLimitsY(0.0 - max / 10, max + max / 10);
@@ -100,7 +98,20 @@ void FSTWindow::showPlots() {
         }
         ImPlot::LinkNextPlotLimits(&plotXLimits.Min, &plotXLimits.Max, nullptr, nullptr);
         ImPlot::PushStyleVar(ImPlotStyleVar_PlotPadding, ImVec2(10, 0));
-        if (ImPlot::BeginPlot(item.name.c_str(), NULL, NULL, ImVec2(-1, 100), ImPlotFlags_NoLegend, NULL,
+        ImGui::PushID(i);
+
+        ImVec2 cursor = ImGui::GetCursorScreenPos();
+        ImGui::Button("  ");
+        ImGui::SetCursorScreenPos(cursor);
+
+        if (ImGui::BeginDragDropSource(ImGuiDragDropFlags_None)) {
+            ImGui::SetDragDropPayload("PlotPayload", &i, sizeof(int));
+            ImGui::Text("Dragging");
+            ImGui::EndDragDropSource();
+        }
+
+        if (ImPlot::BeginPlot(item.name.c_str(), NULL, NULL, ImVec2(-1, 100),
+                              ImPlotFlags_NoLegend | ImPlotFlags_NoChild, NULL,
                               ImPlotAxisFlags_Lock)) {
             ImPlot::PlotStairs(item.name.c_str(), (int *) &item.x_data[0], (int *) &item.y_data[0], item.x_data.size());
             ImPlotLimits limits = ImPlot::GetPlotLimits();
@@ -114,11 +125,11 @@ void FSTWindow::showPlots() {
             ImPlot::PushStyleColor(ImPlotCol_LegendText, ImVec4(0.15, 0.35, 0.15, 1));
             for (int i = 0; i < item.x_data.size(); i++) {
                 std::basic_string<char> value;
-                switch(convertType) {
+                switch (convertType) {
                     case BINARY:
                         value = std::bitset<16>(item.y_data[i]).to_string();
                         //remove all 0 in front
-                        value.erase(0,value.find_first_not_of('0'));
+                        value.erase(0, value.find_first_not_of('0'));
                         break;
                     case DECIMALS:
                         value = std::to_string(item.y_data[i]);
@@ -132,102 +143,32 @@ void FSTWindow::showPlots() {
                 ImPlot::PlotText(value.c_str(), item.x_data[i], item.y_data[i]);
             }
             ImPlot::PopStyleColor();
-
+            ImPlot::PopStyleVar();
             ImPlot::EndPlot();
-            if(ImGui::BeginDragDropSource(ImGuiDragDropFlags_None)){
-                ImGui::SetDragDropPayload("Plot",&i, sizeof(int));
-                ImGui::Text("Dragging");
-                ImGui::EndDragDropSource();
-            }
-            if(ImGui::BeginDragDropTarget()) {
-                if(const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("Plot")) {
-                    int payload_i = *(const int*) payload->Data;
-                    Plot tmp = g_Plots[i];
-                    g_Plots[i] = g_Plots[payload_i];
-                    g_Plots[payload_i] = tmp;
-                }
-                ImGui::EndDragDropTarget();
-            }
-        }
-        ImPlot::PopStyleVar();
-        ImGui::EndChild();
-        ImGui::NewLine();
-    }
-    ImGui::EndGroup();
-/*
-    if (ImGui::TreeNode("Drag and drop to copy/swap items"))
-    {
-        enum Mode
-        {
-            Mode_Copy,
-            Mode_Move,
-            Mode_Swap
-        };
-        static int mode = 0;
-        if (ImGui::RadioButton("Copy", mode == Mode_Copy)) { mode = Mode_Copy; } ImGui::SameLine();
-        if (ImGui::RadioButton("Move", mode == Mode_Move)) { mode = Mode_Move; } ImGui::SameLine();
-        if (ImGui::RadioButton("Swap", mode == Mode_Swap)) { mode = Mode_Swap; }
-        static const char* names[9] =
-                {
-                        "Bobby", "Beatrice", "Betty",
-                        "Brianna", "Barry", "Bernard",
-                        "Bibi", "Blaine", "Bryn"
-                };
-        for (int n = 0; n < IM_ARRAYSIZE(names); n++)
-        {
-            ImGui::PushID(n);
-            if ((n % 3) != 0)
-                ImGui::SameLine();
-            ImGui::Button(names[n], ImVec2(60, 60));
 
-            // Our buttons are both drag sources and drag targets here!
-            if (ImGui::BeginDragDropSource(ImGuiDragDropFlags_None))
-            {
-                // Set payload to carry the index of our item (could be anything)
-                ImGui::SetDragDropPayload("DND_DEMO_CELL", &n, sizeof(int));
-
-                // Display preview (could be anything, e.g. when dragging an image we could decide to display
-                // the filename and a small preview of the image, etc.)
-                if (mode == Mode_Copy) { ImGui::Text("Copy %s", names[n]); }
-                if (mode == Mode_Move) { ImGui::Text("Move %s", names[n]); }
-                if (mode == Mode_Swap) { ImGui::Text("Swap %s", names[n]); }
-                ImGui::EndDragDropSource();
-            }
-            if (ImGui::BeginDragDropTarget())
-            {
-                if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("DND_DEMO_CELL"))
-                {
-                    IM_ASSERT(payload->DataSize == sizeof(int));
-                    int payload_n = *(const int*)payload->Data;
-                    if (mode == Mode_Copy)
-                    {
-                        names[n] = names[payload_n];
-                    }
-                    if (mode == Mode_Move)
-                    {
-                        names[n] = names[payload_n];
-                        names[payload_n] = "";
-                    }
-                    if (mode == Mode_Swap)
-                    {
-                        const char* tmp = names[n];
-                        names[n] = names[payload_n];
-                        names[payload_n] = tmp;
+            if (ImGui::BeginDragDropTarget()) {
+                if (const ImGuiPayload *payload = ImGui::AcceptDragDropPayload("PlotPayload")) {
+                    int payload_i = *(const int *) payload->Data;
+                    for (int k = payload_i; k > i; k--) {
+                        std::swap(g_Plots[k],g_Plots[k-1]);
                     }
                 }
                 ImGui::EndDragDropTarget();
             }
+            ImGui::NewLine();
             ImGui::PopID();
         }
-        ImGui::TreePop();
-    }*/
+    }
+    ImGui::EndGroup();
+    }
 }
 
 void FSTWindow::render() {
     int treeWidth = 250;
     ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
     ImGui::SetNextWindowSize(ImVec2(treeWidth + 500, 500), ImGuiCond_FirstUseEver);
-    ImGui::Begin("PlotWindow", nullptr, ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoScrollWithMouse | ImGuiWindowFlags_MenuBar);
+    ImGui::Begin("PlotWindow", nullptr,
+                 ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoScrollWithMouse | ImGuiWindowFlags_MenuBar);
     {
         if (ImGui::BeginMenuBar()) {
             if (ImGui::BeginMenu("Format")) {
