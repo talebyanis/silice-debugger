f474a5e56993571676ff11700368d1ae85e4da43
Yanis <55869284+shin0bi-y@users.noreply.github.com>
2021-04-22 15:52:23 +0200

    getLines in LogParser + some documentation

--- src/LogParser.cpp
+++ src/LogParser.cpp
@@ -36,6 +36,8 @@ LogParser::LogParser(std::string report_filename)
 
 // ---------------------------------------------------------------------
 
+// Returns a specific column for a line
+// col_nb : 1=filename, 2=token, 3=varname, 4=line, 5=usage
 std::string LogParser::getCol(std::string file_name, std::string var_name, int col_nb)
 {
 	switch (col_nb)
@@ -64,6 +66,7 @@ std::string LogParser::getCol(std::string file_name, std::string var_name, int c
 
 // ---------------------------------------------------------------------
 
+// Returns a list of key (file_name, var_name) having their value's usage matching with the parameter (const, temp...)
 std::list<std::pair<std::string, std::string>> LogParser::getMatch(std::string match)
 {
 	std::list<std::pair<std::string, std::string>> list;
@@ -78,3 +81,16 @@ std::list<std::pair<std::string, std::string>> LogParser::getMatch(std::string m
 }
 
 // ---------------------------------------------------------------------
+
+// ???
+std::pair<int, int> LogParser::getLines(std::string file_name, std::string var_name)
+{
+	for (auto const& [key, val] : this->report_lines)
+	{
+		if (val.filename == file_name && val.varname == var_name)
+		{
+			return std::pair<int, int>(val.line_start, val.line_stop);
+		}
+	}
+	return std::pair(-1, -1);
+}
--- src/LogParser.h
+++ src/LogParser.h
@@ -4,14 +4,24 @@
 #include <map>
 #include <list>
 
+// Stores a line from the report file :
+// FILENAME - TOKEN - VARNAME - LINE - USAGE - INDEX - L_START - L_STOP
 struct report_line {
 	std::string filename;
 	std::string token;
 	std::string varname;
 	std::string line;
 	std::string usage;
+	std::string index;
+	int			line_start;
+	int			line_stop;
 };
 
+/*
+LogParser :
+
+Parses the .v.vio.log file generated after building a design
+*/
 class LogParser
 {
 public:
@@ -19,6 +29,7 @@ public:
 	LogParser(std::string report_filename);
 	std::string getCol(std::string file_name, std::string var_name, int col_nb);
 	std::list<std::pair<std::string, std::string>> getMatch(std::string match);
+	std::pair<int, int> getLines(std::string file_name, std::string var_name);
 private:
 	// (filename, varname) -> report_line
 	std::map<std::pair<std::string, std::string>, report_line> report_lines;
