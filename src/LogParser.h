a383c2f7508571a792bc551a4eb7029325a3d52f
Yanis <55869284+shin0bi-y@users.noreply.github.com>
2021-04-22 09:56:16 +0200

    fix LogParser.h

--- src/LogParser.h
+++ src/LogParser.h
@@ -18,7 +18,7 @@ public:
 	LogParser();
 	LogParser(std::string report_filename);
 	std::string getCol(std::string file_name, std::string var_name, int col_nb);
-	std::list<std::pair<std::string, std::string>> LogParser::getMatch(std::string match);
+	std::list<std::pair<std::string, std::string>> getMatch(std::string match);
 private:
 	// (filename, varname) -> report_line
 	std::map<std::pair<std::string, std::string>, report_line> report_lines;
